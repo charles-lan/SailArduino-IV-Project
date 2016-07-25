31394a2b63b93eec3d1c761cde96adaec20f28d5
Charles Lan <charlis.lan@gmail.com>
2016-07-25 20:18:29 +1200

    Base start

--- SailArduino/Form1.Designer.cs
+++ SailArduino/Form1.Designer.cs
@@ -29,11 +29,141 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.glControl1 = new OpenTK.GLControl();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.ComboCOMPort = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // glControl1
+            // 
+            this.glControl1.BackColor = System.Drawing.Color.Black;
+            this.glControl1.Location = new System.Drawing.Point(21, 306);
+            this.glControl1.Margin = new System.Windows.Forms.Padding(8);
+            this.glControl1.Name = "glControl1";
+            this.glControl1.Size = new System.Drawing.Size(520, 456);
+            this.glControl1.TabIndex = 0;
+            this.glControl1.VSync = false;
+            this.glControl1.Load += new System.EventHandler(this.glControl1_Load);
+            this.glControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.glControl1_Paint);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(54, 64);
+            this.buttonStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(112, 36);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Enabled = false;
+            this.buttonStop.Location = new System.Drawing.Point(214, 62);
+            this.buttonStop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(112, 36);
+            this.buttonStop.TabIndex = 2;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 164);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(1022, 127);
+            this.textBox1.TabIndex = 3;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 230400;
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(615, 329);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 31);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(615, 397);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(142, 31);
+            this.textBox3.TabIndex = 5;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(615, 466);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(142, 31);
+            this.textBox4.TabIndex = 6;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(615, 533);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(142, 31);
+            this.textBox5.TabIndex = 7;
+            // 
+            // ComboCOMPort
+            // 
+            this.ComboCOMPort.FormattingEnabled = true;
+            this.ComboCOMPort.Location = new System.Drawing.Point(431, 62);
+            this.ComboCOMPort.Name = "ComboCOMPort";
+            this.ComboCOMPort.Size = new System.Drawing.Size(141, 33);
+            this.ComboCOMPort.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1064, 736);
+            this.Controls.Add(this.ComboCOMPort);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.glControl1);
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private OpenTK.GLControl glControl1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox ComboCOMPort;
     }
 }
 
--- SailArduino/Form1.cs
+++ SailArduino/Form1.cs
@@ -7,14 +7,211 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+using OpenTK;
 
 namespace SailArduino
 {
+    static class GLDraw
+    {
+        static GLDraw()
+        {
+        }
+
+        public static void Axis()
+        {
+            GL.LineWidth(2f);
+            GL.Begin(PrimitiveType.Lines);
+
+            GL.Color3(1f, 0f, 0f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(50f, 0f, 0f);
+
+            GL.Color3(0f, 1f, 0f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(0f, 50f, 0f);
+
+            GL.Color3(0f, 0f, 1f);
+            GL.Vertex3(0f, 0f, 0f);
+            GL.Vertex3(0f, 0f, 50f);
+
+            GL.End();
+            GL.LineWidth(1f);
+        }
+
+    }
+
     public partial class Form1 : Form
     {
+        bool loaded = false;
+        string RxString;
+        double xrot, yrot, zrot;
+
+
+
         public Form1()
         {
+
             InitializeComponent();
+            GetAvailablePorts();
         }
+
+        private void GetAvailablePorts()
+        {
+            string[] ports = SerialPort.GetPortNames();
+
+        }
+
+        private void glControl1_Load(object sender, EventArgs e)
+        {
+            loaded = true;
+            GL.ClearColor(Color.SkyBlue);
+            SetupViewport();
+
+            GL.Enable(EnableCap.DepthTest);
+        }
+
+        private void SetupViewport()
+        {
+            int w = glControl1.Width;
+            int h = glControl1.Height;
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, w, 0, h, -1, 1); // Bottom-left corner pixel has coordinate (0, 0)
+            GL.Viewport(0, 0, w, h); // Use all of the glControl painting area
+        }
+
+        private void glControl1_Paint(object sender, PaintEventArgs e)
+        {
+            if (!loaded) // Don't start drawing till loaded.
+                return;
+
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            //For now, try draw everything in immediate mode. 
+            GL.Translate(50, 50, 0);
+            
+
+            GL.Color3(Color.Yellow);
+            GL.Begin(PrimitiveType.Triangles);
+            GL.Vertex3(10, 20,10);
+            GL.Vertex3(100, 20,10);
+            GL.Vertex3(100, 50,10);
+            GL.End();
+
+            GLDraw.Axis();
+            glControl1.Invalidate();
+            glControl1.SwapBuffers();
+        }
+
+
+
+
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM15";
+            serialPort1.BaudRate = 230400;
+            serialPort1.Handshake = Handshake.None;
+            serialPort1.Encoding = System.Text.Encoding.Default;
+            serialPort1.DataBits = 8;
+            serialPort1.Parity = Parity.None;
+            serialPort1.DtrEnable = true;
+
+            serialPort1.Open();
+            if (serialPort1.IsOpen)
+            {
+                buttonStart.Enabled = false;
+                buttonStop.Enabled = true;
+            }
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                serialPort1.Close();
+                buttonStart.Enabled = true;
+                buttonStop.Enabled = false;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (serialPort1.IsOpen) serialPort1.Close();
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            RxString = serialPort1.ReadLine();
+            this.Invoke(new EventHandler(UpdateTextLog));
+            this.Invoke(new EventHandler(ProcessString));
+        }
+
+
+        private void UpdateTextLog(object sender, EventArgs e)
+        {
+            // TextBox1 keeps a log of what Strings are coming out
+            textBox1.AppendText(RxString);  
+        }
+
+        private void ProcessString(object sender, EventArgs e)
+        {
+            string rawstring;
+            string[] processed;
+            string[] sentences;
+            string[] sentenceSeperators = new String[] {"$"};
+            string[] stringSeperators = new string[] { ","," "};
+
+            rawstring = RxString;
+
+            sentences = rawstring.Split(sentenceSeperators, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string s in sentences)
+            {
+                processed = s.Split(stringSeperators, StringSplitOptions.RemoveEmptyEntries);
+
+                switch (processed[0])
+                {
+                    //Somewhat inelegant, will figure out something else to do here.
+                    //case "EUX":
+                    //    for (int i = 0; i < processed.Length; i++)
+                    //    {
+
+                    //    }
+                    //    break;
+                    //case "EUY":
+                    //    break;
+                    //case "EUZ":
+                    //    break;
+                    //case "QUAS1":
+                    //    break;
+                    //case "QUAS2":
+                    //    break;
+                    //case "QUAS3":
+                    //    break;
+                    //case "QUAS4":
+                    //    break;
+                    case "QUAS5":
+                        textBox2.Text = processed[1];
+                        textBox3.Text = processed[2];
+                        textBox4.Text = processed[3];
+                        textBox5.Text = processed[4];
+                        break;
+                    case "TIMS1":
+                        break;
+
+
+                    default:
+                        //Skip string if 
+                        break;
+                }
+            }
+        }
+
     }
 }
